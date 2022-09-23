9816e8e3f7737ec08132efb4fa4ff5bc58ed9f47
babykachima <nguyenhoangtuan.workvn@gmail.com>
2022-09-23 18:17:28 +0700

    apply rate speed track player

--- src/Screens/MainScreens/PracticeShadowing.tsx
+++ src/Screens/MainScreens/PracticeShadowing.tsx
@@ -1,53 +1,81 @@
-import React, { useCallback, useEffect, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import Slider from '@react-native-community/slider';
-import Snackbar from 'react-native-snackbar';
 import TrackPlayer, { Capability, usePlaybackState, useProgress, State } from 'react-native-track-player';
-import { StyleSheet, TouchableOpacity, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Image, ScrollView, Modal } from 'react-native';
 import { Header } from '../../Common/Components/Header';
 
 import { useGetDetailDataFireStore } from '../../Hooks/fetchDataFireStore';
-import { ic_pause, ic_play } from '../../Assets';
+import { ic_cancel, ic_pause, ic_play } from '../../Assets';
 import TextCommon from '../../Common/Components/TextCommon';
+import ButtonCustom from '../../Common/Components/ButtonCustom';
+import { listRates } from '../../Utils';
+import { IRate } from '../../Types';
 
 TrackPlayer.updateOptions({
   capabilities: [Capability.Play, Capability.Pause],
   compactCapabilities: [Capability.Play, Capability.Pause],
 });
+interface IModalPopup {
+  visible: boolean;
+  onCloseModal: () => void;
+  onSelectRateItem: () => void;
+}
+const ModalPopup: React.FC<IModalPopup> = ({ visible, onCloseModal, onSelectRateItem }) => {
+  const selectItemRate = (rate: IRate) => {
+    onSelectRateItem(rate);
+    onCloseModal();
+  };
+  return (
+    <Modal animationType="fade" transparent={true} visible={visible}>
+      <View style={styles.modal}>
+        <View style={styles.contentModal}>
+          <TouchableOpacity style={styles.headerContentModal} onPress={onCloseModal}>
+            <Image source={ic_cancel} style={styles.icon} />
+          </TouchableOpacity>
+          {listRates.map((rate) => (
+            <TouchableOpacity style={styles.listRates} key={rate.id} onPress={() => selectItemRate(rate)}>
+              <TextCommon title={rate.rate} containStyles={styles.titleRate} />
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+    </Modal>
+  );
+};
 
 const PracticeShadowing: React.FC = () => {
   const navigation = useNavigation();
   const playBackState = usePlaybackState();
   const [lessionsDetail] = useGetDetailDataFireStore('lessions', 'nsTbaEpUysbeU7IeGG5m');
-  const { position, duration, buffered } = useProgress();
+  const { position, duration } = useProgress();
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [rate, setRate] = useState<IRate>();
+
+  const setupPlayer = useCallback(async () => {
+    try {
+      if (!lessionsDetail?.audio) {
+        return;
+      }
+      await TrackPlayer.setupPlayer();
+      await TrackPlayer.add([
+        {
+          id: 'nsTbaEpUysbeU7IeGG5m',
+          url: lessionsDetail?.audio || '',
+          title: lessionsDetail?.title,
+        },
+      ]);
+    } catch (error) {
+      console.log('setupPlayer error ->', error);
+    }
+  }, [lessionsDetail?.audio, lessionsDetail?.title]);
 
   useEffect(() => {
-    const setupPlayer = async () => {
-      try {
-        if (!lessionsDetail?.audio) {
-          return;
-        }
-        await TrackPlayer.setupPlayer();
-        await TrackPlayer.add([
-          {
-            id: 'nsTbaEpUysbeU7IeGG5m',
-            url: lessionsDetail?.audio || '',
-            title: lessionsDetail?.title,
-          },
-        ]);
-      } catch (error) {
-        Snackbar.show({
-          text: `${error}`,
-          duration: Snackbar.LENGTH_LONG,
-        });
-      }
-    };
     setupPlayer();
     return () => {
-      console.log('clean up setup');
       setupPlayer();
     };
-  }, [lessionsDetail?.audio, lessionsDetail?.title]);
+  }, [setupPlayer]);
 
   const playTrack = useCallback(async () => {
     try {
@@ -66,6 +94,15 @@ const PracticeShadowing: React.FC = () => {
   const handleSlidingComplete = useCallback(async (value: number) => {
     await TrackPlayer.seekTo(value);
   }, []);
+  const handleResetTrack = useCallback(async () => {
+    await TrackPlayer.seekTo(0);
+  }, []);
+  const handleSetSpeed = useCallback(async () => {
+    setModalVisible(true);
+    if (rate?.value) {
+      await TrackPlayer.setRate(rate.value);
+    }
+  }, [rate?.value]);
 
   const convertPosition = useMemo(() => {
     return new Date(position * 1000).toISOString().substr(14, 5);
@@ -74,6 +111,15 @@ const PracticeShadowing: React.FC = () => {
   const convertProgressDuration = useMemo(() => {
     return new Date((duration - position) * 1000).toISOString().substr(14, 5);
   }, [duration, position]);
+
+  const onHandleCloseModal = useCallback(() => {
+    setModalVisible(false);
+  }, []);
+  const onSelectRateItem = useCallback((itemRate: IRate) => {
+    if (itemRate) {
+      setRate(itemRate);
+    }
+  }, []);
 
   return (
     <View style={styles.contain}>
@@ -95,9 +141,9 @@ const PracticeShadowing: React.FC = () => {
             <TextCommon title={convertProgressDuration} containStyles={styles.duration} />
           </View>
           <View style={styles.contentButton}>
-            <View>
-              <TextCommon title="0.25x" />
-            </View>
+            <TouchableOpacity onPress={handleSetSpeed}>
+              <ButtonCustom title={rate?.rate || ''} />
+            </TouchableOpacity>
             <View>
               {playBackState === State.Playing ? (
                 <TouchableOpacity onPress={pauseTrack}>
@@ -109,12 +155,14 @@ const PracticeShadowing: React.FC = () => {
                 </TouchableOpacity>
               )}
             </View>
-            <TouchableOpacity>
-              <Image source={ic_play} style={styles.button} />
+            <TouchableOpacity onPress={handleResetTrack}>
+              <ButtonCustom title="Reset" />
             </TouchableOpacity>
           </View>
         </View>
       </View>
+      {/* Modal */}
+      <ModalPopup visible={modalVisible} onCloseModal={onHandleCloseModal} onSelectRateItem={onSelectRateItem} />
     </View>
   );
 };
@@ -149,6 +197,7 @@ const styles = StyleSheet.create({
   contentButton: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
   },
   button: {
     width: 50,
@@ -166,6 +215,38 @@ const styles = StyleSheet.create({
   duration: {
     fontSize: 16,
     fontWeight: '500',
+  },
+  // Modal
+  modal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(52, 52, 52, 0.8)',
+  },
+  contentModal: {
+    width: '60%',
+    height: 230,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 20,
+  },
+  headerContentModal: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    margin: 10,
+    tintColor: 'red',
+  },
+  listRates: {
+    width: '100%',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleRate: {
+    fontWeight: '600',
   },
 });
 export default PracticeShadowing;
